973056c0e045971ed186bc4fa7a3edd23182cbe9
worjs320 <worjs320@naver.com>
2021-05-03 22:21:57 +0900

    init: initial commit

---
+++ js/index.js
@@ -0,0 +1,645 @@
+var url = 'https://api.upbit.com/v1/market/all';
+var globalData;
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => init(data));
+
+function createTable(jsonData) {
+  var col = ['', '마켓', '현재가', '전일대비', '거래대금'];
+
+  var tableHeader = document.createElement('table');
+  tableHeader.id = 'coinHeader';
+  tableHeader.innerHTML =
+    '<colgroup><col width="25px"><col width="20%"/><col width="55px"/><col width="60px"/><col width="22%"/></colgroup>';
+
+  var thead = tableHeader.createTHead(); // TABLE ROW.
+  var sort = localStorage.getItem('sort')
+    ? localStorage.getItem('sort')
+    : 'down';
+  var sortItem = localStorage.getItem('sortItem')
+    ? localStorage.getItem('sortItem')
+    : 'acc_trade_price_24h';
+
+  for (var i = 1; i < col.length; i++) {
+    var th = document.createElement('th'); // TABLE HEADER.
+    if (i == 1) {
+      th.colSpan = 2;
+      th.innerHTML = col[i] + ' <i class="fas fa-exchange-alt"></i>';
+    } else if (i > 1) {
+      //fa-sort-up fa-sort-down
+      if (i == getSortIndexBySortItem(sortItem)) {
+        th.innerHTML = col[i] + ' <i class="fas fa-sort-' + sort + '"></i>';
+      } else {
+        th.innerHTML = col[i] + ' <i class="fas fa-sort"></i>';
+      }
+    }
+    if (i >= 3) th.style = 'text-align:right';
+    thead.appendChild(th);
+  }
+
+  function getSortIndexBySortItem(sortItem) {
+    switch (sortItem) {
+      case 'trade_price':
+        return 2;
+      case 'signed_change_rate':
+        return 3;
+      case 'acc_trade_price_24h':
+        return 4;
+      default:
+        return 0;
+    }
+  }
+
+  var className = [
+    'bookmark',
+    'market',
+    'trade_price',
+    'percent',
+    'acc_trade_price_24h',
+  ];
+
+  var table = document.createElement('table');
+  table.innerHTML =
+    '<colgroup><col width="25px"><col width="20%"/><col width="55px"/><col width="60px"/><col width="22%"/></colgroup>';
+  table.id = 'coinList';
+  jsonData.sort(function (a, b) {
+    if (sort == 'up') {
+      return parseFloat(a[sortItem]) - parseFloat(b[sortItem]);
+    } else {
+      return parseFloat(b[sortItem]) - parseFloat(a[sortItem]);
+    }
+  });
+
+  for (var i = 0; i < jsonData.length; i++) {
+    tr = table.insertRow(-1);
+    tr.id = jsonData[i][className[1]];
+    var coinBookmarkInfo =
+      JSON.parse(localStorage.getItem('coinBookmarkInfo')) || [];
+    var checked = '';
+
+    for (var j = 0; j < col.length; j++) {
+      var tabCell = tr.insertCell(-1);
+      if (j == 0) {
+        for (var key in coinBookmarkInfo) {
+          if (
+            coinBookmarkInfo[key].market ==
+            jsonData[i][className[1]] + '_bookmark'
+          ) {
+            checked = 'checked';
+            break;
+          } else {
+            checked = '';
+          }
+        }
+        tabCell.innerHTML =
+          '<div class="anim-icon star"><input class="bookmark" type="checkbox" id="' +
+          jsonData[i][className[1]] +
+          '_bookmark' +
+          '"' +
+          checked +
+          '/><label for="' +
+          jsonData[i][className[1]] +
+          '_bookmark' +
+          '"></label></div>';
+      }
+      if (j == 1) {
+        tabCell.innerHTML =
+          '<strong class="' +
+          jsonData[i].market +
+          '">' +
+          '</strong><span>' +
+          jsonData[i].market.split('-')[1] +
+          '/' +
+          jsonData[i].market.split('-')[0] +
+          '</span>';
+      }
+      if (j == 3) {
+        tabCell.innerHTML =
+          '<p class="signed_change_rate"></p><span class="signed_change_price"></span>';
+      }
+      tabCell.className = className[j];
+    }
+  }
+
+  document.getElementById('tableHeader').appendChild(tableHeader);
+  document.getElementById('tableBody').appendChild(table);
+}
+
+function openUpbitPage() {
+  var win = window.open(
+    'https://upbit.com/exchange?code=CRIX.UPBIT.' + this.className,
+    '_blank'
+  );
+  win.focus();
+}
+
+function init(jsonData) {
+  var marketString = '';
+  for (i = 0; i < jsonData.length; i++) {
+    if (jsonData[i].market.substr(0, 3) != 'KRW') {
+      jsonData.splice(i, 1);
+      i--;
+    }
+  }
+
+  for (i = 0; i < jsonData.length; i++) {
+    marketString += jsonData[i].market;
+    if (i < jsonData.length - 1) marketString += ',';
+  }
+
+  globalData = jsonData;
+  fetch('https://api.upbit.com/v1/ticker?markets=' + marketString)
+    .then((response) => response.json())
+    .then((data) => createTable(data))
+    .then(() => getMarketName())
+    .then(() => customAddEventListener())
+    .then(() => webSocketConfig());
+}
+
+function getMarketName() {
+  var lang = localStorage.getItem('lang');
+  if (lang == null) {
+    localStorage.setItem('lang', 'korean_name');
+    lang = 'korean_name';
+  }
+
+  for (var i = 0; i < globalData.length; i++) {
+    document
+      .getElementById(globalData[i].market)
+      .getElementsByClassName(globalData[i].market)[0].innerHTML =
+      globalData[i][lang];
+  }
+}
+
+function setMarketName() {
+  var lang = localStorage.getItem('lang');
+  if (lang == 'korean_name') {
+    lang = 'english_name';
+    localStorage.setItem('lang', lang);
+  } else if (lang == 'english_name') {
+    lang = 'korean_name';
+    localStorage.setItem('lang', lang);
+  }
+
+  for (var i = 0; i < globalData.length; i++) {
+    document
+      .getElementById(globalData[i].market)
+      .getElementsByClassName(globalData[i].market)[0].innerHTML =
+      globalData[i][lang];
+  }
+}
+
+function webSocketConfig() {
+  var wsUri = 'wss://api.upbit.com/websocket/v1';
+  websocket = new WebSocket(wsUri);
+  websocket.binaryType = 'arraybuffer';
+
+  websocket.onopen = function (evt) {
+    onOpen(evt);
+  };
+
+  websocket.onclose = function (evt) {
+    onClose(evt);
+  };
+
+  websocket.onmessage = function (evt) {
+    onMessage(evt);
+  };
+
+  websocket.onerror = function (evt) {
+    onError(evt);
+  };
+}
+
+function onOpen(evt) {
+  var code = [];
+  for (i = 0; i < globalData.length; i++) {
+    code.push(globalData[i].market);
+  }
+
+  var msg = [
+    { ticket: 'test' },
+    {
+      type: 'ticker',
+      codes: code,
+    },
+  ];
+
+  msg = JSON.stringify(msg);
+  websocket.send(msg);
+}
+
+function onClose() {
+  webSocketConfig();
+}
+
+function onMessage(evt) {
+  var enc = new TextDecoder('utf-8');
+  var arr = new Uint8Array(evt.data);
+  var jsonData = JSON.parse(enc.decode(arr));
+  var privPrice = document
+    .getElementById(jsonData.code)
+    .getElementsByClassName('trade_price')[0].textContent;
+
+  document
+    .getElementById(jsonData.code)
+    .getElementsByClassName(
+      'trade_price'
+    )[0].innerHTML = jsonData.trade_price.toLocaleString();
+  document
+    .getElementById(jsonData.code)
+    .getElementsByClassName(
+      'signed_change_rate'
+    )[0].innerHTML = getPercentNumber(jsonData.signed_change_rate);
+
+  var currentPrice = jsonData.trade_price.toLocaleString();
+  if (privPrice != currentPrice && privPrice != '') {
+    if (privPrice < currentPrice) {
+      document
+        .getElementById(jsonData.code)
+        .getElementsByClassName('trade_price')[0]
+        .classList.remove('price-down');
+      document
+        .getElementById(jsonData.code)
+        .getElementsByClassName('trade_price')[0]
+        .classList.add('price-up');
+    } else if (privPrice > currentPrice) {
+      document
+        .getElementById(jsonData.code)
+        .getElementsByClassName('trade_price')[0]
+        .classList.remove('price-up');
+      document
+        .getElementById(jsonData.code)
+        .getElementsByClassName('trade_price')[0]
+        .classList.add('price-down');
+    }
+  }
+  document
+    .getElementById(jsonData.code)
+    .getElementsByClassName(
+      'signed_change_price'
+    )[0].innerHTML = getPlusMinusNumber(jsonData.signed_change_price);
+
+  document
+    .getElementById(jsonData.code)
+    .getElementsByClassName('acc_trade_price_24h')[0].innerHTML = getNumberUnit(
+    jsonData.acc_trade_price_24h
+  );
+  document.getElementById(jsonData.code).className = jsonData.change;
+}
+
+function getPlusMinusNumber(theNumber) {
+  if (theNumber > 0) {
+    return (
+      '+' +
+      theNumber.toLocaleString(navigator.language, {
+        maximumFractionDigits: 2,
+      })
+    );
+  } else {
+    return theNumber.toLocaleString(navigator.language, {
+      maximumFractionDigits: 2,
+    });
+  }
+}
+
+function getNumberUnit(theNumber) {
+  var result = theNumber.toLocaleString('ko-KR', {
+    maximumFractionDigits: 0,
+  });
+  if (result.length >= 8)
+    result = result.substring(0, result.length - 8) + '백만';
+  return result;
+}
+
+function getPercentNumber(theNumber) {
+  if (theNumber > 0) {
+    return '+' + (theNumber * 100).toFixed(2) + '%';
+  } else {
+    return (theNumber * 100).toFixed(2) + '%';
+  }
+}
+
+function onError() {
+  webSocketConfig();
+}
+
+function searchCoin() {
+  var input = document.getElementById('searchCoinInput');
+  var filter = input.value.toUpperCase();
+  var choFilter = cho_hangul(input.value.toUpperCase());
+  var table = document.getElementById('coinList');
+  var tr = table.getElementsByTagName('tr');
+  var searchBookmarkFlag = document.getElementById('searchBookmark').checked;
+  var bookmark;
+
+  for (var i = 0; i < tr.length; i++) {
+    for (var j = 1; j < 2; j++) {
+      td = tr[i].getElementsByTagName('td')[1];
+      bookmark = tr[i]
+        .getElementsByTagName('td')[0]
+        .getElementsByTagName('div')[0]
+        .getElementsByTagName('input')[0].checked;
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        txtChoValue = cho_hangul(td.textContent) || cho_hangul(td.innerText);
+        if (
+          txtValue.toUpperCase().indexOf(filter) > -1 ||
+          (txtChoValue.toUpperCase().indexOf(choFilter) == 0 && isCho(filter))
+        ) {
+          if (searchBookmarkFlag && bookmark) {
+            tr[i].style.display = '';
+            break;
+          } else if (!searchBookmarkFlag) {
+            tr[i].style.display = '';
+            break;
+          }
+        } else {
+          tr[i].style.display = 'none';
+        }
+      }
+    }
+  }
+}
+
+function isCho(str) {
+  var pattern = /[ㄱ-ㅎ]/gi;
+  return pattern.test(str);
+}
+
+function cho_hangul(str) {
+  cho = [
+    'ㄱ',
+    'ㄲ',
+    'ㄴ',
+    'ㄷ',
+    'ㄸ',
+    'ㄹ',
+    'ㅁ',
+    'ㅂ',
+    'ㅃ',
+    'ㅅ',
+    'ㅆ',
+    'ㅇ',
+    'ㅈ',
+    'ㅉ',
+    'ㅊ',
+    'ㅋ',
+    'ㅌ',
+    'ㅍ',
+    'ㅎ',
+  ];
+  result = '';
+  for (i = 0; i < str.length; i++) {
+    code = str.charCodeAt(i) - 44032;
+    if (code > -1 && code < 11172) result += cho[Math.floor(code / 588)];
+    else result += str.charAt(i);
+  }
+  return result;
+}
+
+function onCheckBookmark() {
+  stopColorEvent();
+  var el = this;
+  if (el.checked == true) {
+    var coinBookmarkInfo =
+      JSON.parse(localStorage.getItem('coinBookmarkInfo')) || [];
+    coinBookmarkInfo.push({ market: el.id });
+    localStorage.setItem('coinBookmarkInfo', JSON.stringify(coinBookmarkInfo));
+  } else {
+    var coinBookmarkInfo =
+      JSON.parse(localStorage.getItem('coinBookmarkInfo')) || [];
+    for (var key in coinBookmarkInfo) {
+      if (coinBookmarkInfo[key].market == el.id) {
+        coinBookmarkInfo.splice(key, 1);
+      }
+    }
+    localStorage.setItem('coinBookmarkInfo', JSON.stringify(coinBookmarkInfo));
+    if (document.getElementById('searchBookmark').checked == true) {
+      document.getElementById(el.id.split('_')[0]).style.display = 'none';
+      var onCheckCoinBookmarkInfo =
+        JSON.parse(localStorage.getItem('coinBookmarkInfo')) || [];
+      if (onCheckCoinBookmarkInfo.length == 0) createNoneBookmarkTextCell();
+    }
+  }
+}
+
+function onCheckBookmarkSearch(el) {
+  document.getElementById('searchCoinInput').value = '';
+  var input, table, tr, checked, i;
+  input = document.getElementById('searchCoinInput');
+  table = document.getElementById('coinList');
+  tr = table.getElementsByTagName('tr');
+  var noneBookmark = true;
+  if (el.checked == true) {
+    for (i = 0; i < tr.length; i++) {
+      checked = tr[i]
+        .getElementsByTagName('td')[0]
+        .getElementsByTagName('div')[0]
+        .getElementsByTagName('input')[0].checked;
+      if (checked == true) {
+        tr[i].style.display = '';
+        noneBookmark = false;
+      } else {
+        tr[i].style.display = 'none';
+      }
+    }
+    localStorage.setItem('bookmarkStatus', true);
+  } else {
+    for (i = 0; i < tr.length; i++) {
+      tr[i].style.display = '';
+    }
+    localStorage.removeItem('bookmarkStatus');
+  }
+  if (noneBookmark && el.checked) {
+    createNoneBookmarkTextCell();
+  } else if (!el.checked && document.getElementById('noBookmark') != null) {
+    document.getElementById('noBookmark').remove();
+  }
+}
+
+function createNoneBookmarkTextCell() {
+  var row = document.getElementById('coinHeader').insertRow(-1);
+  var cell = row.insertCell(-1);
+  row.id = 'noBookmark';
+  cell.colSpan = 5;
+  cell.style = 'text-align:center; padding: 30px 0;';
+  cell.innerHTML =
+    '즐겨찾기한 코인이 없습니다.<br/><a id="showCoinListBtn">코인 목록</a>';
+  document
+    .getElementById('showCoinListBtn')
+    .addEventListener('click', function () {
+      onCheckBookmarkSearch({ checked: false });
+      document.getElementById('searchBookmark').checked = false;
+    });
+}
+
+function stopColorEvent() {
+  for (
+    var i = 0;
+    i < document.getElementsByClassName('trade_price').length;
+    i++
+  ) {
+    document
+      .getElementsByClassName('trade_price')
+      [i].classList.remove('price-up');
+    document
+      .getElementsByClassName('trade_price')
+      [i].classList.remove('price-down');
+  }
+}
+
+function customAddEventListener() {
+  document
+    .getElementById('searchBookmark')
+    .addEventListener('click', function () {
+      onCheckBookmarkSearch(this);
+    });
+  document
+    .getElementById('searchCoinInput')
+    .addEventListener('keyup', searchCoin);
+  document
+    .getElementById('searchCoinInput')
+    .addEventListener('search', searchCoin);
+  var checkboxes = document.querySelectorAll(
+    'input[type=checkbox][class=bookmark]'
+  );
+
+  for (var checkbox of checkboxes) {
+    checkbox.addEventListener('change', onCheckBookmark);
+  }
+
+  var links = document.querySelectorAll('strong');
+
+  for (var link of links) {
+    link.addEventListener('click', openUpbitPage);
+  }
+
+  var bookmarkStatus =
+    JSON.parse(localStorage.getItem('bookmarkStatus')) || false;
+  if (bookmarkStatus) {
+    document.getElementById('searchBookmark').checked = true;
+    var eventFocus = new Event('click');
+    document.getElementById('searchBookmark').dispatchEvent(eventFocus);
+  }
+
+  var myTable = document.getElementById('coinList');
+  var replace = replacement(myTable);
+  function sortTD(index) {
+    stopColorEvent();
+    replace.ascending(index);
+  }
+  function reverseTD(index) {
+    stopColorEvent();
+    replace.descending(index);
+  }
+
+  function setSort(thisObj, index) {
+    for (var i = 0; i < document.getElementsByTagName('th').length; i++) {
+      if (i != index - 1 && i != 0)
+        document
+          .getElementsByTagName('th')
+          [i].getElementsByTagName('i')[0].className = 'fas fa-sort';
+    }
+    if (thisObj.getElementsByTagName('i')[0].classList.contains('fa-sort-up')) {
+      reverseTD(index);
+      thisObj
+        .getElementsByTagName('i')[0]
+        .classList.replace('fa-sort-up', 'fa-sort-down');
+      localStorage.setItem('sort', 'down');
+      localStorage.setItem('sortItem', getSortItemByIndex(index));
+    } else if (
+      thisObj.getElementsByTagName('i')[0].classList.contains('fa-sort-down')
+    ) {
+      sortTD(index);
+      thisObj
+        .getElementsByTagName('i')[0]
+        .classList.replace('fa-sort-down', 'fa-sort-up');
+      localStorage.setItem('sort', 'up');
+      localStorage.setItem('sortItem', getSortItemByIndex(index));
+    } else {
+      reverseTD(index);
+      thisObj
+        .getElementsByTagName('i')[0]
+        .classList.replace('fa-sort', 'fa-sort-down');
+      localStorage.setItem('sort', 'down');
+      localStorage.setItem('sortItem', getSortItemByIndex(index));
+    }
+  }
+
+  function getSortItemByIndex(index) {
+    switch (index) {
+      case 2:
+        return 'trade_price';
+      case 3:
+        return 'signed_change_rate';
+      case 4:
+        return 'acc_trade_price_24h';
+      default:
+        return 'none';
+    }
+  }
+
+  document.getElementsByTagName('th')[0].addEventListener('click', function () {
+    setMarketName();
+  });
+  document.getElementsByTagName('th')[1].addEventListener('click', function () {
+    var thisObj = this;
+    setSort(thisObj, 2);
+  });
+  document.getElementsByTagName('th')[2].addEventListener('click', function () {
+    var thisObj = this;
+    setSort(thisObj, 3);
+  });
+  document.getElementsByTagName('th')[3].addEventListener('click', function () {
+    var thisObj = this;
+    setSort(thisObj, 4);
+  });
+  SimpleScrollbar.initEl(document.querySelector('#tableBody'));
+}
+
+function colorModePreview(ele) {
+  if (ele.checked == true) {
+    localStorage.setItem('color-theme', 'dark');
+    document.getElementsByTagName('body')[0].classList = 'dark-preview';
+  } else {
+    localStorage.setItem('color-theme', 'light');
+    document.getElementsByTagName('body')[0].classList = 'white-preview';
+  }
+}
+
+window.onload = function () {
+  var isUserColorTheme = localStorage.getItem('color-theme');
+  var isOsColorTheme = window.matchMedia('(prefers-color-scheme: dark)').matches
+    ? 'dark'
+    : 'light';
+
+  var getUserTheme = isUserColorTheme ? isUserColorTheme : isOsColorTheme;
+
+  if (getUserTheme === 'dark') {
+    document.getElementsByClassName('btn-container')[0].innerHTML =
+      '<i class="fa fa-sun-o" aria-hidden="true"></i>' +
+      '<label id="btn-color-mode-switch" class="switch btn-color-mode-switch">' +
+      '<input type="checkbox" name="color_mode" id="color_mode" value="1" checked/>' +
+      '<label for="color_mode" class="btn-color-mode-switch-inner"></label></label>' +
+      '<i class="fa fa-moon-o" aria-hidden="true"></i>';
+    localStorage.setItem('color-theme', 'dark');
+    document.documentElement.setAttribute('color-theme', 'dark');
+    document.getElementsByTagName('body')[0].classList = 'dark-preview';
+  } else {
+    document.getElementsByClassName('btn-container')[0].innerHTML =
+      '<i class="fa fa-sun-o" aria-hidden="true"></i>' +
+      '<label id="btn-color-mode-switch" class="switch btn-color-mode-switch">' +
+      '<input type="checkbox" name="color_mode" id="color_mode" value="1" />' +
+      '<label for="color_mode" class="btn-color-mode-switch-inner"></label></label>' +
+      '<i class="fa fa-moon-o" aria-hidden="true"></i>';
+    localStorage.setItem('color-theme', 'light');
+    document.documentElement.setAttribute('color-theme', 'light');
+    document.getElementsByTagName('body')[0].classList = 'white-preview';
+  }
+  document.getElementById('color_mode').addEventListener('change', function () {
+    colorModePreview(this);
+  });
+
+  document.getElementById('searchCoinInput').focus();
+};
